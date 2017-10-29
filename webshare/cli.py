e3d12a6e315419df52ae54bfc1ae149cd799ea7c
Tomas Krizek <tomas.krizek@mailbox.org>
2017-10-29 14:43:10 +0100

    cli: add link command
    Signed-off-by: Tomas Krizek <tomas.krizek@mailbox.org>

--- webshare/cli.py
+++ webshare/cli.py
@@ -12,6 +12,11 @@ def search(args):
         print("{:2d}. {}".format((i+1), file))
 
 
+def get_link(args):
+    data = api.file_link(args.id)
+    print(data)
+
+
 def main():
     parser = argparse.ArgumentParser(description='webshare.cz API client')
     subparsers = parser.add_subparsers(help='choose a subcomand',
@@ -21,9 +26,9 @@ def main():
     search_parser.add_argument(
         'what', type=str, nargs='+', help='string identifying the file')
 
-#    link_parser = subparsers.add_parser('link', help='get downloadable link')
-#    link_parser.add_argument(
-#        'what', type=str, nargs='+', help='string identifying the file')
+    link_parser = subparsers.add_parser('link', help='get downloadable link')
+    link_parser.add_argument(
+        'id', type=str, help='ID of the file')
 
     args = parser.parse_args()
     if args.subparser is None:
@@ -32,6 +37,7 @@ def main():
 
     functions = {
         'search': search,
+        'link': get_link,
     }
     functions[args.subparser](args)
 
