5e2e57ebc3812b1a4b87687b60318d0df3b62709
Tomas Krizek <tomas.krizek@mailbox.org>
2017-11-04 21:12:58 +0100

    query: add normalization and extensions
    Signed-off-by: Tomas Krizek <tomas.krizek@mailbox.org>

---
+++ tests/test_commands.py
@@ -0,0 +1,32 @@
+import pytest
+
+from webshare import commands
+
+
+@pytest.mark.parametrize("query, expected", [
+    ('', ''),
+    ('a', 'a'),
+    ('a ', 'a'),
+    ('A', 'a'),
+    ('a b B', 'a b'),
+    ('a b c B', 'a b c'),
+])
+def test_normalize_query(query, expected):
+    assert commands.normalize_query(query) == expected
+
+
+@pytest.mark.parametrize("query, options, expected", [
+    ('a', None, ['a']),
+    ('a', [], ['a']),
+    ('a', [''], ['a']),
+    ('a', ['a'], ['a']),
+    ('A', ['a'], ['a']),
+    ('a', ['x'], ['a x']),
+    ('a', ['x a'], ['a x']),
+    ('a', ['x y'], ['a x y']),
+    ('a', ['x', 'y'], ['a x', 'a y']),
+    ('a b', ['x'], ['a b x']),
+    ('a bc', ['a', 'c'], ['a bc', 'a bc c']),
+])
+def test_query_expand(query, options, expected):
+    assert commands.query_expand(query, options) == expected
--- webshare/commands.py
+++ webshare/commands.py
@@ -20,3 +20,24 @@ def search(what):
                 results.append(file_)
 
     return results
+
+
+def normalize_query(query):
+    """Lowercase words and use only unique ones"""
+    normalized = []
+    for word in query.split(' '):
+        word = word.lower()
+        if word and word not in normalized:
+            normalized.append(word)
+    return ' '.join(normalized)
+
+
+def query_expand(query, options=None):
+    """Create multiple queries by extending it by some options"""
+    query = normalize_query(query)
+    if not options:
+        return [query]
+    results = []
+    for option in options:
+        results.append(normalize_query(query + ' ' + option))
+    return results
