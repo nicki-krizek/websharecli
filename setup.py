18c6ea16dc3652311f2a87649c8716fb432ad42f
Tomas Krizek <tomas.krizek@mailbox.org>
2021-03-01 22:22:45 +0100

    release 2.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools  # noqa
 
 setup(
     name="websharecli",
-    version="2.0.1",
+    version="2.0.2",
     description="webshare.cz CLI downloader",
     author="Tomas Krizek",
     author_email="tomas.krizek@mailbox.org",
