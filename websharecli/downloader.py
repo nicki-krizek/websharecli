d8e4567a53c6a93d311609684d23b78875e5c1e9
rada-dev <vaclavradazb@gmail.com>
2024-03-05 18:06:57 +0100

    2.2.3, download progress made more eye candy, showing RETRY/FAIL/SUCCESS of the download

--- websharecli/downloader.py
+++ websharecli/downloader.py
@@ -12,16 +12,16 @@ from websharecli.exceptions import TooManyDownloadRetriesException
 from websharecli import config
 
 
-def download_url(url, output_path, tor, tor_port, retries=3, timeout=10, i=1, n=1):
+def download_url(url, output_path, tor, tor_port, i=1, n=1, retries=3, timeout=10):
     session, original_ip, tor_ip = make_requests_tor_session(tor, tor_port)
     if tor:
         # print(f"{T.green}Downloading file through tor proxies (http / https) {' / '.join(session.proxies.values())}"
         #       f", original ip: {original_ip}; tor ip: {tor_ip}{T.normal}", file=sys.stderr)
-        pbar_desc = f"{T.cyan}{i}/{n} {T.green}TOR:{T.normal} {os.path.basename(output_path)}"
+        pbar_desc = f"{i}/{n} {T.magenta}(TOR){T.normal} {os.path.basename(output_path)}"
         assert original_ip != tor_ip, "Traffic is not going through tor. Exit."
     else:
         # print(f"{T.yellow}Downloading file without tor, your ip: {original_ip}{T.normal}", file=sys.stderr)
-        pbar_desc = f"{T.cyan}{i}/{n}{T.normal} {os.path.basename(output_path)}"
+        pbar_desc = f"{i}/{n}{T.normal} {os.path.basename(output_path)}"
     response = session.get(url, stream=True)
     total_size_in_bytes = int(response.headers.get('content-length', 0))
     block_size = config.CONFIG.chunk_size  # ~1 KB
@@ -43,9 +43,9 @@ def download_url(url, output_path, tor, tor_port, retries=3, timeout=10, i=1, n=
         url = file_link_by_id(ident)
         retries -= 1
         if retries > 0:
-            progress_bar.desc = f"{T.red}RETRY{T.normal} "+progress_bar.desc
+            progress_bar.desc = f"{T.red}RETRY{T.normal} " + progress_bar.desc
             progress_bar.close()
-            return download_url(i, n, url, output_path, tor, tor_port, retries, timeout)
+            return download_url(url, output_path, tor, tor_port, i, n, retries, timeout)
         else:
             # print("Download incomplete. No more retries.", file=sys.stderr)
             progress_bar.desc = f"{T.red}FAIL{T.normal} " + progress_bar.desc
@@ -60,13 +60,15 @@ def download_url(url, output_path, tor, tor_port, retries=3, timeout=10, i=1, n=
 def download_urls(urls, output_folder, tor, tor_ports, pool_size):
     output_paths = list(map(lambda x: os.path.join(output_folder, filename_from_url(x)), urls))
     output_paths = distinguish_filenames(output_paths)
-    tors = [tor]*len(urls)
+    tors = [tor] * len(urls)
     ports_for_each_url = repeat_list_to_length(tor_ports, len(urls))
+    i_list = range(1, len(urls) + 1)
+    n_list = [len(urls)] * len(urls)
     with ThreadPoolExecutor(max_workers=pool_size) as executor:
         # Submit tasks to the executor
         futures = [executor.submit(download_url, url, output_path, tor, tor_port, i, n)
                    for (url, output_path, tor, tor_port, i, n) in
-                   zip(urls, output_paths, tors, ports_for_each_url, range(1, len(urls)+1), [len(urls)]*len(urls))]
+                   zip(urls, output_paths, tors, ports_for_each_url, i_list, n_list)]
         # Use as_completed to get the results as they are completed
         for future in as_completed(futures):
             result = future.result()
